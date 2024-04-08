fde47aaed9999649033051b959de6e8bc1ee78f4
assilbekov <askhat.assilbekov@gmail.com>
2024-04-08 13:39:23 -0300

    add transition to ProjectNavigation

--- src/components/ProjectNavigation.tsx
+++ src/components/ProjectNavigation.tsx
@@ -22,6 +22,10 @@ export const ProjectNavigation = ({ isOpen }: ProjectNavigationProps) => {
       initial="close"
       animate="open"
       exit="close"
+      transition={{
+        duration: 0.5,
+        ease: "easeInOut",
+      }}
       className={`flex flex-col gap-8 h-screen w-64 absolute bg-neutral-900 ml-0 border-r border-neutral-800 p-5 ${
         isOpen ? "left-64" : "left-20"
       }`}
