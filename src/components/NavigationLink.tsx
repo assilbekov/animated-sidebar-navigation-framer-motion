0ecbf8e67586946bf4670eb0211332d9f3059ca5
assilbekov <askhat.assilbekov@gmail.com>
2024-04-08 12:56:43 -0300

    add NavigationLink style

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,19 +1,19 @@
-
-
 interface NavigationLinkProps {
   label: React.ReactNode;
   icon: React.ReactNode;
-  href: string;
+  href?: string;
 }
 
 export const NavigationLink = ({ href, icon, label }: NavigationLinkProps) => {
   return (
-    <a 
-    href={href}
-    className=""
+    <a
+      href={href}
+      className="flex p-1 rounded cursor-pointer stroke-[0.75] hover:stroke-slate-50 stroke-slate-200 text-slate-200 hover:text-slate-50 place-items-center gap-3 hover:bg-slate-700/30 transition-colors duration-100"
     >
       {icon}
-      <p>{label}</p>
+      <p className="text-inherit overflow-clip whitespace-nowrap tracking-wide">
+        {label}
+      </p>
     </a>
-  )
-}
+  );
+};
