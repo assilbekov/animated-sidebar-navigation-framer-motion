5cc486cb3648dd493208dd6f79d570018bab7c3e
assilbekov <askhat.assilbekov@gmail.com>
2024-04-08 12:41:25 -0300

    add svg animation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -20,12 +20,23 @@ const containerVariants = {
   },
 };
 
+const svgVariants = {
+  close: {
+    rotate: 360,
+  },
+  open: {
+    rotate: 180,
+  },
+};
+
 export const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
   const containerAnimationContrors = useAnimationControls();
+  const svgAnimationControls = useAnimationControls();
 
   useEffect(() => {
     containerAnimationContrors.start(isOpen ? "open" : "close");
+    svgAnimationControls.start(isOpen ? "open" : "close");
   }, [isOpen]);
 
   const toggleNavigation = () => setIsOpen(!isOpen);
@@ -40,11 +51,14 @@ export const Navigation = () => {
       <div className="flex flex-row w-full justify-between place-items-center">
         <div className="w-10 h-10 bg-gradient-to-br from-orange-500 to-amber-700 rounded-full"></div>
         <button className="p-1 rounded-full flex" onClick={toggleNavigation}>
-          <svg
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
+            variants={svgVariants}
+            animate={svgAnimationControls}
+            transition={{ duration: 0.5, type: "spring" }}
             stroke="currentColor"
             className="w-6 h-6 stroke-neutral-200"
           >
@@ -53,7 +67,7 @@ export const Navigation = () => {
               strokeLinejoin="round"
               d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
             />
-          </svg>
+          </motion.svg>
         </button>
       </div>
     </motion.nav>
